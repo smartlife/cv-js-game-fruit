b1f1d417e554f974f03a490114b30fd4332973d8
smartlife <medleonid@yahoo.com>
2025-06-08 12:45:08 +0400

    add several levels

--- js/levelConfig.js
+++ js/levelConfig.js
@@ -2,12 +2,45 @@ import { FRUITS } from './fruitConfig.js';
 
 export const LEVELS = [
   {
-    speed: 0.3,
+    speed: 0.5,
     fruits: [
       { type: 'apple', priority: 1 },
       { type: 'pear', priority: 0.5 },
+      { type: 'pineapple', priority: 1.5 },
+      { type: 'mandarine', priority: 0.5 },
     ],
   },
+  
+  {
+    speed: 0.6,
+    fruits: [
+      { type: 'apple', priority: 1 },
+      { type: 'pear', priority: 0.5 },
+      { type: 'pineapple', priority: 1.2 },
+      { type: 'mandarine', priority: 0.7 },
+    ],
+  },
+  
+  {
+    speed: 0.7,
+    fruits: [
+      { type: 'apple', priority: 1 },
+      { type: 'pear', priority: 0.5 },
+      { type: 'pineapple', priority: 1.1 },
+      { type: 'mandarine', priority: 0.8 },
+    ],
+  },    
+
+  {
+    speed: 0.8,
+    fruits: [
+      { type: 'apple', priority: 1 },
+      { type: 'pear', priority: 0.5 },
+      { type: 'pineapple', priority: 1.0 },
+      { type: 'mandarine', priority: 0.9 },
+    ],
+  },    
+
 ];
 
 // utility to pick a fruit based on priorities
